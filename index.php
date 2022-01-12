850ac2bed50c0452e67f783ad869f434f426207d
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-12 13:53:50 +0100

    tableaux et tableaux associatifs

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 // Premiere ligne
 
-$fruits = array("Apple", "Banana" "Orange");
-echo ;
+$fruits = array("Apple", "Banana" ,"Orange");
+echo count($fruits) ."<br>";
+echo $fruits[1];
+echo "<br><br>";
 
 //Deuxieme ligne
+$age = array("Peter"=> "35", "Ben"=> "37", "Joe"=> "43");
 
+echo "<br><br>Ben is " . $age["Ben"] . " years old.<br><br>";
 
-$age = array("Peter" "35", "Ben" "37", "Joe" "43");
-
-echo "<br><br>Ben is " .  . " years old.<br><br>";
-
-($age  $x  $y) {
-    echo "<br>Key=" .  . ", Value=" . ;
+foreach($age as $x => $y) {
+    echo "<br>Key=" . $x  . ", Value=" .$y ;
 }
 
 
@@ -21,13 +21,16 @@ echo "<br><br>Ben is " .  . " years old.<br><br>";
 //Troisieme ligne
 
 $colors = array("red", "green", "blue", "yellow");
-;
+sort($colors);
+echo "<br><br>";
 
 
 // Quatrieme ligne
 
 $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-;
+rsort($age);
+echo "<br><br>";
+asort($age);
 
 
 
